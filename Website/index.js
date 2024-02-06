ceb85fd21bff7672e44c71fb86e8db8d3f4588a0
infinite-kyle <143848131+infinite-kyle@users.noreply.github.com>
2024-02-06 10:58:28 -1000

    Delete Website directory
    moved files to main directory, deleting directory

--- Website/index.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("https://hexxukvzdg.execute-api.us-east-1.amazonaws.com/default/cloud-resume-challenge-view-counter");
-    let data = await response.json();
-    counter.innerHTML = `Visitors: ${data} `;
-}
-
-updateCounter();
